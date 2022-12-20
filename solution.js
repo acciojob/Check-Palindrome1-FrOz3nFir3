3e4ef5c40f9b6ba88e1aa366e9f7f10548027075
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-12-20 13:44:33 +0530

    optimized approach

--- solution.js
+++ solution.js
@@ -1,6 +1,23 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(string){
+  // optimized approach O(N) Time and O(1) Space
+  let lowerCased = string.toLowerCase();
 
+  let startingPointer = 0;
+  let endingPointer = lowerCased.length -1;
+
+  // palindrome is same as forward and backwards thus checking first character and last character and repeating simultaneously for other part of the strings.
+  while(startingPointer < endingPointer){
+    let currentStartingCharacter = lowerCased[startingPointer];
+    let currentEndingCharacter = lowerCased[endingPointer];
+     
+    if(currentStartingCharacter != currentEndingCharacter) return false;
+     
+    startingPointer++;
+    endingPointer--;
+  }
+
+  return true;
 }
 module.exports = palindrome
